be756bcafa1ab7d1c048a73301eb89a9d780025a
Pooja Sibal <contact.poojasibal@gmail.com>
2018-11-03 12:53:18 +1100

    Initial Commit

---
+++ petClinicApp/src/main/java/com/myApp/petClinicApp/PetClinicAppApplication.java
@@ -0,0 +1,12 @@
+package com.myApp.petClinicApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicAppApplication.class, args);
+	}
+}
---
+++ petClinicApp/src/test/java/com/myApp/petClinicApp/PetClinicAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myApp.petClinicApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinicAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
