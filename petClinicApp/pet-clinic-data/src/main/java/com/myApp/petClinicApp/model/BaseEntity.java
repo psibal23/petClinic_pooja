75c82227de37636a16979ff258f7c8568855e3eb
Pooja Sibal <contact.poojasibal@gmail.com>
2018-11-10 00:02:35 +1100

    Added the Id in models

---
+++ petClinicApp/pet-clinic-data/src/main/java/com/myApp/petClinicApp/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.myApp.petClinicApp.model;
+
+
+public class BaseEntity {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+
+}
